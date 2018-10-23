107f10ca61cc3250dce78e9554fe2762c5b24495
Lev <levr@iguaz.io>
2018-10-23 20:10:32 +0300

    2 funcs

---
+++ func1/main.go
@@ -0,0 +1,28 @@
+package main
+
+type Demo struct {
+	Name string  `csv:"name"`
+	ID   int     `csv:"ID"`
+	Num  float64 `csv:"number"`
+}
+
+func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
+	tab := []Demo{
+		Demo{
+			Name: "some name",
+			ID:   1,
+			Num:  42.5,
+		},
+	}
+
+	err := csvtag.DumpToFile(tab, "csv_file_name.csv")
+	if err != nil {
+		return nuclio.Response{
+			StatusCode: 500,
+		}, nil
+	}
+
+	return nuclio.Response{
+		StatusCode: 201,
+	}, nil
+}
